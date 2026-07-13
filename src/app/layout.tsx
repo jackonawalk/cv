96d56c036c66f0be193283891efa77d3cd952fc1
Jack Sellwood <8140402+jackonawalk@users.noreply.github.com>
2026-07-13 08:48:38 -0500

    add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Jack Sellwood",
@@ -25,9 +25,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.className}>
-      <body>{children}</body>
-      <Analytics />
-      <SpeedInsights/>
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
